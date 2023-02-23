41e67ee3a56b977ca619af1c15cd1672d7a43e77
clement <clementchpoon@gmail.com>
2023-02-23 22:13:14 +0000

    add optimisation

--- build_em.sh
+++ build_em.sh
@@ -1,4 +1,5 @@
-DEMANGLE_SUPPORT=1 emcc main.cpp notes.cpp gsdl.cpp -s WASM=1 -s USE_SDL=2 -s USE_SDL_MIXER=2 -s USE_SDL_TTF=2 -o pacman.html -DEMCXX -O0 -g  --preload-file "./font.ttf" --shell-file=./template.html
+emcc main.cpp notes.cpp gsdl.cpp -s WASM=1 -s USE_SDL=2 -s USE_SDL_MIXER=2 -s USE_SDL_TTF=2 -o pacman.html -DEMCXX -O3  --preload-file "./font.ttf" --shell-file=./template.html
+
 
 
 
